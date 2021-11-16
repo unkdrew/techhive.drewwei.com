9d93dc079d030dd5858dfb8bb58de422dc354b27
Drew Wei <drewwei@amazon.com>
2021-11-15 20:57:04 -0600

    Integrated with GoogleAdsense.

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,7 +1,39 @@
-/**
- * Implement Gatsby's SSR (Server Side Rendering) APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/ssr-apis/
- */
+import React from 'react';
 
-// You can delete this file if you're not using it
+export const onRenderBody = ({ setHeadComponents, setPostBodyComponents }) => {
+  const pluginOptions = {
+    googleAdClientId: `ca-pub-9760840662732376`,
+    head: true  // Set to false if you prefer to have your adsense script loaded at the end of body instead of head.
+  };
+
+  if (process.env.NODE_ENV !== 'production') {
+    return null;
+  }
+
+  if (pluginOptions.googleAdClientId === undefined) {
+    return null;
+  }
+
+  const setComponents = pluginOptions.head
+    ? setHeadComponents
+    : setPostBodyComponents;
+
+  return setComponents([
+    <script
+      async
+      type="text/javascript"
+      src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
+    />,
+    <script
+      key={'gatsby-plugin-google-adsense'}
+      dangerouslySetInnerHTML={{
+        __html: `
+        (adsbygoogle = window.adsbygoogle || []).push({
+            google_ad_client: "${pluginOptions.googleAdClientId}",
+            enable_page_level_ads: true
+        });
+        `
+      }}
+    />
+  ]);
+};
